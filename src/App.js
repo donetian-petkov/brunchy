73a98424c6e8d6244b5d37a8aa2834aba516428a
donetian-petkov <donetian+petkov@gmail.com>
2024-09-27 00:26:58 +0300

    update(Cart): CSS Styling and Layout

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import {Hero} from "./components/Hero/Hero";
 import {Header} from "./components/Header/Header";
 import {MenuList} from "./components/MenuList/MenuList";
 import {Cart} from "./components/Cart/Cart";
+import {useState} from "react";
 
 function App() {
+
+    const [cart, setCart] = useState([]);
+
     return (
         <div className="App">
             <Header/>
             <Hero/>
-            <MenuList/>
-            <Cart/>
+            <MenuList setCart={setCart}/>
+            <Cart cart={cart}/>
         </div>
     );
 }
